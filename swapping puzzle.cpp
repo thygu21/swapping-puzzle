6f7623ca8cf699936195440e4ada1b85d444f55a
thygu21 <thygu21@gmail.com>
2019-05-27 21:29:35 +0900

    project finish

--- swapping puzzle.cpp
+++ swapping puzzle.cpp
@@ -1,29 +1,22 @@
 #include <iostream>
 #include <string>
+#include <vector>
 #include <math.h>
 
 using namespace std;
 
 void swap(int value)
 {
-	//print value
-
-	//print path
+	vector<int> a;
 	int x = value;
-	int lineC = 0; //20 to enter
+
+	//find path
 	for (int i = 0; i < value; i++) // 2 to 4 if value=3
 	{
 		for (int j = 0; j < i + 2; j++)
 		{
-			cout << x << " ";
-			lineC++;
+			a.push_back(x);
 			x += 2 * int(pow(-1, i));
-
-			if (lineC == 20)
-			{
-				cout << "\n";
-				lineC = 0;
-			}
 		}
 		x -= 1 * int(pow(-1, i));
 	}
@@ -32,18 +25,19 @@ void swap(int value)
 	{
 		for (int j = 0; j < value - i; j++)
 		{
-			cout << x << " ";
-			lineC++;
-			x -= 2 * int(pow(-1, i)) * int(pow(-1,value - 1));
-			if (lineC == 20)
-			{
-				cout << "\n";
-				lineC = 0;
-			}
+			a.push_back(x);
+			x -= 2 * int(pow(-1, i)) * int(pow(-1, value - 1));
 		}
-		x += 1 * int(pow(-1, i)) * int(pow(-1, value - 1));
-		if (value % 2 == 0 && i % 2 == 0)
-			x -= 2;
+		x += 3 * int(pow(-1, i)) * int(pow(-1, value-1));
+	}
+
+	//print
+	cout << a.size();
+	for (int i = 0; i < a.size(); i++)
+	{
+		if (i % 20 == 0)
+			cout << "\n";
+		cout << a[i] << " ";
 	}
 	cout << "\n";
 }
@@ -64,34 +58,3 @@ int main()
 	}
 }
 
-// n부터 2씩 한번증가 -> n+2 = a
-// n+2보다 1큰 n+3부터 2씩 두번 감소 -> n-1 = b
-// n-1보다 1작은 n-2부터 2씩 세번 증가 -> n+6
-// 거꾸로
-// b다시 출력
-// a다시 출력
-// n+1출력
-
-//2 3 4 3 2 1 -> 3
-//2 3 4 5 4 3 2 1 -> 4
-
-//3
-//3 5/ 6 4 2 /1 3 5 7 /6 4 2 /3 5 /4
-
-//4
-//4 6/ 7 5 3 /2 4 6 8 /9 7 5 3 1 /2 4 6 8 /7 5 3 /4 6 /5
-
-// 1 2 -> 3 -> 3 5 6 4 2 1 3 5 7 6 4 2 3 5 4 
-// 3 3
-// 3 2
-// 1
-
-// 1 2 -> 2
-// 2 2
-// 1
-
-// 1 2 -> 4
-// 3 4
-// 4 4
-// 3 2
-// 1
